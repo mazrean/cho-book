cd7eb9232b126819a36fde20010488ede7fffb8f
mazrean <shunsuke.w04@gmail.com>
2023-07-25 03:51:33 +0900

    fix auth

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,13 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import Google from '@auth/core/providers/google';
-import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
+import type { Handle } from '@sveltejs/kit';
+import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, AUTH_SECRET } from '$env/static/private';
 
-export const handle = SvelteKitAuth({
-	providers: [Google({ clientId: GOOGLE_CLIENT_ID, clientSecret: GOOGLE_CLIENT_SECRET })],
-	secret: 'secret',
-	trustHost: true
-});
+export const handle = SvelteKitAuth(async (event) => {
+	const authOptions = {
+		providers: [Google({ clientId: GOOGLE_CLIENT_ID, clientSecret: GOOGLE_CLIENT_SECRET })],
+		secret: AUTH_SECRET,
+		trustHost: true
+	};
+	return authOptions;
+}) satisfies Handle;
