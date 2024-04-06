15e5161fd007c83bf88bf95ac1e785d9d44d89c5
mazrean <shunsuke.w04@gmail.com>
2024-04-07 03:24:55 +0900

    loading

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -85,9 +85,7 @@
     {#each ownBooks as book}
         <BookItem book={book} on:delete={onDelete} />
     {/each}
-    {#if isLoading}
-        <div class="uk-align-center" data-uk-spinner="ratio:3"></div>
-    {/if}
+    <div class="uk-align-center" data-uk-spinner="ratio:3" style={isLoading ? "": "opacity: 0"}></div>
     <BarCodeModal open={modalOpen} ownBookMap={new Map(ownBooks?.map(book => [book.isbn, book]))} on:submit={onSubmit} />
 {:else}
     <button class="uk-button uk-button-default uk-button-primary" on:click={() => signIn('google')}>Sign In</button>
