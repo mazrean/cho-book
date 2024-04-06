a999dfff9c402e925b87e9c21bda10ab072069fe
mazrean <shunsuke.w04@gmail.com>
2024-04-07 02:00:24 +0900

    fix images

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -19,7 +19,13 @@ export async function load({ platform, locals }) {
 	const {
 		results: books
 	}: {
-		results: Book[];
+		results: {
+			isbn: string;
+			title: string;
+			author: string;
+			publisher: string;
+			img_url: string;
+		}[];
 	} = await db
 		.prepare(
 			'SELECT books.* FROM books JOIN user_book_relations ON books.isbn = user_book_relations.book_isbn WHERE user_book_relations.user_email = ? ORDER BY user_book_relations.created_at DESC LIMIT ?'
@@ -29,7 +35,16 @@ export async function load({ platform, locals }) {
 	const isEnd = books.length < limit;
 
 	return {
-		ownBooks: books,
+		ownBooks: books.map(
+			(book) =>
+				({
+					isbn: book.isbn,
+					title: book.title,
+					author: book.author,
+					publisher: book.publisher,
+					imgUrl: book.img_url
+				} as Book)
+		),
 		limit,
 		isEnd
 	};
