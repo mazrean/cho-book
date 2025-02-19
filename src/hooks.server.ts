0763be03e18421d066fb3fbf22f5c42b5b9fa816
mazrean <shunsuke.w04@gmail.com>
2025-02-19 16:16:16 +0900

    debug configにbasePath設定

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,9 +2,14 @@ import { SvelteKitAuth } from '@auth/sveltekit';
 import Google from '@auth/core/providers/google';
 import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, AUTH_SECRET } from '$env/static/private';
 
-export const { handle } = SvelteKitAuth({
+const { handle: handle2 } = SvelteKitAuth({
 	providers: [Google({ clientId: GOOGLE_CLIENT_ID, clientSecret: GOOGLE_CLIENT_SECRET })],
 	secret: AUTH_SECRET,
 	trustHost: true,
 	basePath: '/auth'
 });
+
+export const handle = (input) => {
+	console.log('input', input);
+	return handle2(input);
+};
